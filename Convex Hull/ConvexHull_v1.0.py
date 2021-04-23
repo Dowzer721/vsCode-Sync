a1f782bba396e5dd7a1fb67b6cc9dd0b6d5e211f
Luke Shears <Dowzer721@users.noreply.github.com>
2021-04-23 17:38:44 +0100

    Made some changes to "ConvexHull_v1.0.py", but it isn't working.

--- Convex Hull/ConvexHull_v1.0.py
+++ Convex Hull/ConvexHull_v1.0.py
@@ -5,6 +5,7 @@ from random import randint
 
 screenW, screenH = 600, 500
 screen = pygame.display.set_mode((screenW, screenH))
+screen.fill((0, 0, 0))
 
 pointCount = 16
 points = []
@@ -15,9 +16,6 @@ class Point:
         self.position = (x_, y_)
 
         self.connectedPoint = None
-    
-    def connectToPoint(self, pt):
-        self.connectedPoint = pt
     
     def render(self, renderColour=(200, 0, 0), renderRadius=4):
         pygame.draw.circle(
@@ -50,7 +48,6 @@ for pt in points:
 # currentID = lowestXID
 # currentAngle = 3 * pi / 2.0
 
-# screen.fill((255, 255, 255))
 # for _ in range(10):
 
 #     anglesToAllOtherPoints = []
@@ -104,54 +101,124 @@ for pt in points:
 #     currentAngle = atan2(dy, dx)
 #     currentID = minDifferenceID
 
-averageX = sum(pt.position[0] for pt in points) / float(pointCount)
-averageY = sum(pt.position[1] for pt in points) / float(pointCount)
+averageX = int(sum(pt.position[0] for pt in points) / float(pointCount))
+averageY = int(sum(pt.position[1] for pt in points) / float(pointCount))
 
 pygame.draw.circle(
     screen, (0, 255, 0),
-    (int(averageX), int(averageY)), 8,
+    (averageX, averageY), 8,
     0
 )
 
-vertexCount = 64
 vertexList = []
-for c in range(vertexCount):
-    n = (c + 1) % vertexCount
-    thetaC = (2.0 * pi / vertexCount) * c
-    thetaN = (2.0 * pi / vertexCount) * n
+divisionCount = 3
+for c in range(divisionCount):
+    thetaC = ((2.0 * pi / divisionCount) * c) + (2.0 * pi)
+    thetaC %= (2.0 * pi)
+    
+    n = (c + 1) % divisionCount
+    thetaN = ((2.0 * pi / divisionCount) * n) + (2.0 * pi)
+    thetaN %= (2.01 * pi)
+
+    pygame.draw.line(
+        screen, (0, 0, 200),
+        (averageX, averageY), (
+            averageX + int(cos(thetaC) * averageX * 0.5), 
+            averageY + int(sin(thetaC) * averageY * 0.5)            
+        ), 
+        1
+    )
 
     maxDist = -1
     maxDistID = 0
     for pt in points:
-        dx = averageX - pt.position[0]
-        dy = averageY - pt.position[1]
-        angleToPoint = atan2(dy, dx)
-        if (angleToPoint > thetaC) and (angleToPoint < thetaN):
+        dx = pt.position[0] - averageX
+        dy = pt.position[1] - averageY
+        angle = (atan2(dy, dx) + (2.0 * pi)) % (2.0 * pi)
+        
+        if angle > thetaC and angle < thetaN:
+        
             dist = ((dx**2) + (dy**2)) ** 0.5
+            # x2 = averageX + int(cos(angle) * dist)
+            # y2 = averageY + int(sin(angle) * dist)
+            # pygame.draw.line(
+            #     screen, (150, 150, 150),
+            #     (averageX, averageY),
+            #     (x2, y2), 1
+            # )
+            # break
+
             if dist > maxDist:
                 maxDist = dist
                 maxDistID = pt.id
     
+    # pygame.draw.line(
+    #     screen, (150, 150, 150),
+    #     (averageX, averageY), (points[maxDistID].position[0], points[maxDistID].position[1]),
+    #     1
+    # )
+
     vertexList.append(points[maxDistID])
 
 for c in range(len(vertexList)):
-    n = (c + 1) % vertexCount
-
-    x1 = vertexList[c].position[0]
-    y1 = vertexList[c].position[1]
-    x2 = vertexList[n].position[0]
-    y2 = vertexList[n].position[1]
-
+    n = (c + 1) % len(vertexList)
     pygame.draw.line(
-        screen, (0, 0, 255),
-        (x1, y1), (x2, y2),
+        screen, (150, 150, 0),
+        points[c].position,
+        points[n].position,
         1
     )
+
+
+# vertexCount = 64
+# vertexList = []
+# for c in range(vertexCount):
+#     n = (c + 1) % vertexCount
+#     thetaC = (2.0 * pi / vertexCount) * c
+#     thetaN = (2.0 * pi / vertexCount) * n
+
+#     maxDist = -1
+#     maxDistID = 0
+#     for pt in points:
+#         dx = averageX - pt.position[0]
+#         dy = averageY - pt.position[1]
+#         angleToPoint = atan2(dy, dx)
+#         if (angleToPoint > thetaC) and (angleToPoint < thetaN):
+#             dist = ((dx**2) + (dy**2)) ** 0.5
+#             if dist > maxDist:
+#                 maxDist = dist
+#                 maxDistID = pt.id
+    
+#     vertexList.append(points[maxDistID])
+
+# for c in range(len(vertexList)):
+#     n = (c + 1) % vertexCount
+
+#     x1 = vertexList[c].position[0]
+#     y1 = vertexList[c].position[1]
+#     x2 = vertexList[n].position[0]
+#     y2 = vertexList[n].position[1]
+
+#     pygame.draw.line(
+#         screen, (0, 0, 255),
+#         (x1, y1), (x2, y2),
+#         1
+#     )
 
 
 
 
 # while(True):
+#     screen.fill((0, 0, 0))
+
+#     for ev in pygame.event.get():
+#         if ev.type == pygame.MOUSEBUTTONUP:
+#             pos = pygame.mouse.get_pos()
+
+#             dx = pos[0] - averageX
+#             dy = pos[1] - averageY
+#             angle = (atan2(dy, dx) + (2.0 * pi)) % (2.0 * pi)
+#             print(f"angle: {angle}")
 
 for pt in points:
     pt.render()
