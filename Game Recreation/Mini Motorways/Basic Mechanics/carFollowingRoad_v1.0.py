5b936f2a87c2ae958b3866317210781b38d3f870
Luke Shears <Dowzer721@users.noreply.github.com>
2021-12-08 21:57:50 +0000

    Splitting the angles at junctions. Stuck with acute/obtuse angles

--- Game Recreation/Mini Motorways/Basic Mechanics/carFollowingRoad_v1.0.py
+++ Game Recreation/Mini Motorways/Basic Mechanics/carFollowingRoad_v1.0.py
@@ -49,14 +49,14 @@ class Vector:
 # Where the road starts, travels through and ends on the canvas:
 roadPoints = [
     Vector(canvasW * 0.3, canvasH * 0.1),
-    Vector(canvasW * 0.4, canvasH * 0.6),
+    Vector(canvasW * 0.5, canvasH * 0.6),
     Vector(canvasW * 0.7, canvasH * 0.3),
     Vector(canvasW * 0.9, canvasH * 0.9),
     Vector(canvasW * 0.2, canvasH * 0.9),
     Vector(canvasW * 0.1, canvasH * 0.1)
 ]
 # Creating this to save on calling the "len" method repeatedly:
-roadPointsLen = len(roadPoints)
+numberOfRoadPoints = len(roadPoints)
 
 # The width of the road:
 roadWidth = 16
@@ -66,21 +66,21 @@ roadColour = (100, 100, 100)
 
 # Draw the road to the canvas:
 def renderRoad():
-    for i in range(roadPointsLen):
+    for i in range(numberOfRoadPoints):
 
-        x1, y1 = roadPoints[i].toTuple()
+        x1, y1 = roadPoints[i].toInt()
 
-        # pygame.draw.circle(
-        #     canvas, 
-        #     roadColour, 
-        #     roadPoints[i].toInt(),
-        #     roadWidth, 
-        #     0
-        # )
-        # if i == roadPointsLen - 1: break
+        pygame.draw.circle(
+            canvas, 
+            roadColour, 
+            (x1, y1),
+            roadWidth, 
+            0
+        )
+        # if i == numberOfRoadPoints - 1: break
 
 
-        x2, y2 = roadPoints[(i + 1) % roadPointsLen].toTuple()
+        x2, y2 = roadPoints[(i + 1) % numberOfRoadPoints].toTuple()
         dx = x2 - x1
         dy = y2 - y1
         angle = atan2(dy, dx)
@@ -96,21 +96,108 @@ def renderRoad():
             0
         )
 
-    for i in range(roadPointsLen):
+    # for i in range(numberOfRoadPoints):
+    #     pygame.draw.line(
+    #         canvas, 
+    #         (255,255,255), 
+    #         roadPoints[i].toTuple(), 
+    #         roadPoints[(i+1)%numberOfRoadPoints].toTuple(), 
+    #         1
+    #     )
+
+    # My previous method involved drawing the roads from each point, and then just drawing a middle 
+    # line, which worked visually, but had no way for the vehicle to know which side of the road was 
+    # which. 
+    # 
+    # My new method is to draw both sides of the road as separate entities, so that the vehicles know 
+    # which side is which, and have a reference point for travelling along the roads and around 
+    # corners etc. 
+
+    for c in range(numberOfRoadPoints):
+        p = (c - 1 + numberOfRoadPoints) % numberOfRoadPoints
+        n = (c + 1) % numberOfRoadPoints
+
+        # # p_dx = roadPoints[c].x - roadPoints[p].x
+        # # p_dy = roadPoints[c].y - roadPoints[p].y
+        p_dx = roadPoints[p].x - roadPoints[c].x
+        p_dy = roadPoints[p].y - roadPoints[c].y
+        p_angle = (atan2(p_dy, p_dx) + (2 * pi)) % (2 * pi)
+        # p_angle = atan2(p_dy, p_dx) + (2 * pi)
+
+        n_dx = roadPoints[n].x - roadPoints[c].x
+        n_dy = roadPoints[n].y - roadPoints[c].y
+        n_angle = (atan2(n_dy, n_dx) + (2 * pi)) % (2 * pi)
+        # n_angle = atan2(n_dy, n_dx) + (2 * pi)
+
+        # mid_angle = (p_angle + n_angle) / 2
+        
+        # print(f"{p} -> {c} @ {round(p_angle, 2)}; {c} -> {n} @ {round(n_angle, 2)}; mid_angle:{round(mid_angle,2)}")
+        # print(f"{c} -> {p} @ {round(p_angle, 2)}; {c} -> {n} @ {round(n_angle, 2)}; mid_angle:{round(mid_angle,2)}")
+
+        # _______________________________
+        # Let's imagine the triangle ABC:
+        #           A
+        #           .-------------- O[RIGIN]
+        #          / \
+        #         /   \
+        #      C /_____\ B
+        #
+        # The angle OAB is approx 1.05r, and angle OAC is approx 2.09r.
+        # Visually, we can clearly see that the angle between the two would be straight down vertically, 
+        # bisecting line BC, which would be the average of the two angles, or (OAB + OAC) / 2. 
+        # However, in practice, this doesn't always work. I am not entirely sure why, but I do have a 
+        # solution:
+        #
+        # If we choose an arbitrary distance, and then head in the direction of B from A but that distance, 
+        # and note the point. Now we repeat the same, from A to C by the same distance and note this point 
+        # too. 
+        # Now we have two points, equidistant from A. 
+        # If we find the mid-point between these two, and then find the angle from A to the mid-point, 
+        # we should have the angle which bisects the angle CAB.
+
+        arbitraryDistance = roadWidth * 4
+
+        pointA_x, pointA_y = roadPoints[c].toTuple()
+        pointA_x += cos(n_angle) * arbitraryDistance
+        pointA_y += sin(n_angle) * arbitraryDistance
+
+        pointB_x, pointB_y = roadPoints[c].toTuple()
+        pointB_x += cos(p_angle) * arbitraryDistance
+        pointB_y += sin(p_angle) * arbitraryDistance
+
+        midPoint_x = (pointA_x + pointB_x) / 2
+        midPoint_y = (pointA_y + pointB_y) / 2
+
+        bisectingAngle = atan2(
+            midPoint_y - roadPoints[c].y,
+            midPoint_x - roadPoints[c].x
+        )
+
+        # Drawing the bisecting angle:
         pygame.draw.line(
-            canvas, 
-            (255,255,255), 
-            roadPoints[i].toTuple(), 
-            roadPoints[(i+1)%roadPointsLen].toTuple(), 
+            canvas,
+            (255, 0, 0),
+            (midPoint_x, midPoint_y),
+            roadPoints[c].toInt(),
             1
         )
+
+        # Maybe my approach here isn't worth the hassle, because the game is fixed to a grid.
+
+        
+
+
+
+# renderRoad()
+# exit(0)
+
 
 class Vehicle:
     def __init__(self):
 
-        roadStartingIndex = randint(0, roadPointsLen - 1)
-        previousIndex = (roadStartingIndex + roadPointsLen - 1) % roadPointsLen
-        nextIndex = (roadStartingIndex + 1) % roadPointsLen
+        roadStartingIndex = randint(0, numberOfRoadPoints - 1)
+        previousIndex = (roadStartingIndex + numberOfRoadPoints - 1) % numberOfRoadPoints
+        nextIndex = (roadStartingIndex + 1) % numberOfRoadPoints
 
         # print(f"{previousIndex} : {roadStartingIndex} : {nextIndex}")
 
@@ -144,7 +231,7 @@ class Vehicle:
         # )
 
         # startingRoadPoint = roadPoints[roadStartingIndex]
-        # nextRoadPoint = roadPoints[(roadStartingIndex + 1) % roadPointsLen]
+        # nextRoadPoint = roadPoints[(roadStartingIndex + 1) % numberOfRoadPoints]
 
         # dx = nextRoadPoint.x - startingRoadPoint.x
         # dy = nextRoadPoint.y - startingRoadPoint.y
@@ -210,6 +297,6 @@ while True:
 
     renderRoad()
     
-    car.render()
+    # car.render()
 
     pygame.display.flip()
