ca57584ba000fc8d302fd1bc4339eca300c1a6b0
Luke Shears <48263786+Dowzer721@users.noreply.github.com>
2024-06-28 20:34:13 +0100

    Update solver_v2_0.py
    Implemented "pencils" into the solver.

--- Sudoku Solver/solver_v2_0.py
+++ Sudoku Solver/solver_v2_0.py
@@ -1,18 +1,23 @@
 
 """
-This doesn't currently work, because I am not sure how I actually want to go about testing each cell. 
-But I have written the functions to allow for extracting a given column/row/segment from the Grid.
+I would instead like to perform the same tests that a human does to solve the puzzle, but implementing these tests in code has so far been challenging. I wonder if implementing the ability to "pencil" in numbers into the cells would be helpful...
+---
+I have now implemented the same basic tests as a human would do, using the ability to "pencil" in numbers into each cell. 
+Currently, each cell has a list of pencil numbers, which get removed if they appear in the adjacent column/row/segment, and then if there is only one pencil number remaining, it is set as the Penned number.
 
-I don't think that it is a good idea to simply check the validity of every column/row/segment for 
-each value tested in every empty cell, as I fear this will take ages. 
-
-I would instead like to perform the same tests that a human does to solve the puzzle, but implementing 
-these test in code has so far been challenging. I wonder if implementing the ability to "pencil" in 
-numbers into the cells would be helpful...
+This works, but next, I would like to begin checking for single occurrences of each Pencilled number within each column/row/segment, and then setting the Pen number to that single occurrence.
 
 """
 
-# Grid = [
+
+Pens = [
+  [4,3,0,0],
+  [1,2,3,0],
+  [0,0,2,0],
+  [2,1,0,0]
+]
+
+# Pens = [
 #   [9,0,0,2,0,0,0,0,0],
 #   [0,0,0,8,4,0,2,6,0],
 #   [0,0,7,0,0,3,0,5,0],
@@ -24,26 +29,25 @@ numbers into the cells would be helpful...
 #   [0,0,0,0,0,8,3,0,0]
 # ]
 
-Grid = [
-  [4,3,0,0],
-  [1,2,3,0],
-  [0,0,2,0],
-  [2,1,0,0]
-]
+gridSize = len(Pens[0])
 
-colCount = rowCount = len(Grid[0])
-print(f"c:{colCount}, r:{rowCount}")
+segmentSize = int(gridSize ** 0.5)
+# print(segmentSize)
 
-segmentSize = int(colCount ** 0.5)
-print(segmentSize)
+Pencils = []
+for r in range(gridSize):
+  PencilRow = []
+  for c in range(gridSize):
+    PencilRow.append([i for i in range(1, gridSize+1)])
+  Pencils.append(PencilRow)
 
-def printGrid():
+def printPens():
   print(" ", end='')
   for _ in range(segmentSize):
     print("_"*((segmentSize*2)+1), end=' ')
   print()
   
-  for r in range(rowCount):
+  for r in range(gridSize):
     print("|", end='')
     digitGap = ' '
     if r % segmentSize == segmentSize-1: digitGap = '_'
@@ -51,12 +55,13 @@ def printGrid():
     for segmentColumn in range(segmentSize):
       startCol = segmentColumn*segmentSize
       endCol = (segmentColumn+1)*segmentSize
-      print(digitGap + digitGap.join([str(n) for n in Grid[r][startCol:endCol]]) + digitGap, end='')
+      print(digitGap + digitGap.join([str(n) for n in Pens[r][startCol:endCol]]) + digitGap, end='')
       if endCol % segmentSize == 0: print('|', end='')
       if endCol % (segmentSize**2) == 0: print()
   
   print()
   print()
+
 
 def flatten(list_):
   flat = []
@@ -65,12 +70,12 @@ def flatten(list_):
   return flat
 
 def getRow(r):
-  return Grid[r]
+  return Pens[r]
 
 def getCol(c):
-  return [Grid[r][c] for r in range(rowCount)]
+  return [Pens[r][c] for r in range(gridSize)]
 
-def getSegment(c,r):
+def getSeg(r,c):
   # If the grid is divided into segments, which column is the segment in?
   segmentColumn = int(c / segmentSize)
   
@@ -87,51 +92,58 @@ def getSegment(c,r):
   
   Segment = []
   for row in range(startRow, endRow):
-    Segment.append(Grid[row][startColumn:endColumn])
+    Segment.append(Pens[row][startColumn:endColumn])
   
   Segment = flatten(Segment)
   
   return Segment
 
-seg = getSegment(0,0)
-# segflat = [row for row in seg]
-# segflat = flatten(seg)
+def updatePencils(r, c):
+  if Pens[r][c] > 0: 
+    Pencils[r][c] = [0 for _ in range(gridSize)]
+    return
+  
+  Row = getRow(r)
+  Col = getCol(c)
+  Seg = getSeg(r, c)
 
-print(seg)
+  for i in range(1, gridSize+1):
+    if (i in Row) or (i in Col) or (i in Seg):
+      Pencils[r][c][i-1] = 0
 
-def segmentValid(Segment):
-  if Segment.count(0) > 0: return False
 
-def rowValid(Row):
-  ValueCounts = [Row.count(i) for i in range(1, colCount+1)]
-  for Count in ValueCounts:
-    if Count > 1: return False
 
-def colValid(Column):
-  ValueCounts = [Column.count(i) for i in range(1, rowCount+1)]
-  for Count in ValueCounts:
-    if Count > 1: return False
 
-for r in range(rowCount):
-  rowCopy = [v for v in getRow(r)]
-  for c in range(colCount):
-    colCopy = [v for v in getCol(c)]
-    segmentCopy = [v for v in getSegment(c, r)]
-    originalValue = Grid[r][c]
-    for i in range(1, colCount+1):
-      
-      rowCopy[c] = i
-      colCopy[r] = i
-      segmentCopy[c + (r*segmentSize)] = i
-      
-      rv = rowValid(rowCopy)
-      cv = colValid(colCopy)
-      sv = segmentValid(segmentCopy)
-      
-      if (rv and cv and sv): continue
-      
-      rowCopy[c] = originalValue
-      colCopy[r] = originalValue
-      segmentCopy[c + (r*segmentSize)] = originalValue
-      
-    print(rowCopy)
+print("Starting Grid:")
+printPens()
+# for Row in Pencils:
+#   print(Row)
+# print()
+
+# How many penned numbers need to be written into the grid:
+PensToWrite = sum([Row.count(0) for Row in Pens])
+
+for _ in range(PensToWrite):
+  
+  for r in range(gridSize):
+    for c in range(gridSize):
+      updatePencils(r, c)
+      if Pencils[r][c].count(0) == (gridSize-1):
+        Pens[r][c] = [v for v in Pencils[r][c] if v > 0][0]
+    
+
+print("Final Grid:")
+printPens()
+
+# for Row in Pencils:
+#   print(Row)
+# print()
+
+
+
+
+
+
+
+
+
